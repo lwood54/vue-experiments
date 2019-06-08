e62f38c83114f753262f4873ebe73448cb06fa13
Logan Wood <lwood3499@gmail.com>
2019-06-08 17:24:29 -0500

    test clone results title change

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
 	<div id="app">
 		<div id="nav">
 			<router-link to="/">Home</router-link>|
-			<router-link to="/cities-match">Cities-Match</router-link>
+			<router-link to="/cities-match">Cities: Matching</router-link>
 		</div>
 		<router-view/>
 	</div>
